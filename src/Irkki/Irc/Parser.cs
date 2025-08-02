40dea0dfdc0d093135609acb3dc74e69cd157c71
Stefan Alfbo <stefan@alfbo.se>
2025-08-02 17:02:05 +0000

    Start to implement the parser by using the lexer

--- src/Irkki/Irc/Parser.cs
+++ src/Irkki/Irc/Parser.cs
@@ -2,5 +2,21 @@ namespace Irkki.Irc;
 
 public class Parser
 {
-    
+    private readonly Lexer _lexer;
+
+    public Parser(Lexer lexer)
+    {
+        _lexer = lexer;
+    }
+
+    public Message ParseMessage()
+    {
+        string? prefix = null;
+        if (_lexer.NextToken().Type == TokenType.Colon)
+        {
+            prefix = _lexer.NextToken().Value;
+        }
+
+        return new Message(prefix, "not implemented yet", Array.Empty<string>());
+    }
 }
---
+++ tests/Irkki.Tests/Irc/ParserFixture.cs
@@ -0,0 +1,36 @@
+using Irkki.Irc;
+
+namespace Irkki.Tests.Irc;
+
+public class ParserFixture
+{
+    [Fact]
+    public void TestExtractingPrefixFromMessage()
+    {
+        // Arrange
+        var message = ":copper.libera.chat NOTICE * :*** Checking Ident\r\n";
+        var lexer = new Lexer(message);
+        var parser = new Parser(lexer);
+
+        // Act
+        var parsedMessage = parser.ParseMessage();
+
+        // Assert
+        Assert.Equal("copper.libera.chat", parsedMessage.Prefix);
+    }
+
+    [Fact]
+    public void TestMessageWithoutPrefix()
+    {
+        // Arrange
+        var message = "NOTICE * :*** Checking Ident\r\n";
+        var lexer = new Lexer(message);
+        var parser = new Parser(lexer);
+
+        // Act
+        var parsedMessage = parser.ParseMessage();
+
+        // Assert
+        Assert.Null(parsedMessage.Prefix);
+    }
+}
